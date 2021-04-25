d345a51ba933e54064ef435a472b9f96e2878437
Kibet 1998 <ronoronald1998@gmail.com>
2021-04-25 14:31:30 +0300

    add Js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function orderPizza(){
+    total();
+   
+
+}
+function total(){
+    var typeOfPizza = document.getElementById("pizza").value;
+    var quantity = document.getElementById("quantity").value;
+    var topping = document.getElementById("toppings").value;
+    var crust = document.getElementById("crust").value;
+    var size = document.getElementById("size").value;
+    var total = (parseInt(topping) + parseInt(crust) + parseInt(size)) * quantity;
+    document.getElementById("Pizza").innerHTML = typeOfPizza;
+    document.getElementById("Quantity").innerHTML = quantity;
+    document.getElementById("Size").innerHTML = size;
+    document.getElementById("Crust").innerHTML = crust;
+    document.getElementById("Toppings").innerHTML = topping;
+    document.getElementById("Total").innerHTML = total;
+
+   
+
+}
